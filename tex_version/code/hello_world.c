18730e762f8c991e55d4f3a93b43901d6b03833d
caubert <caubert@augusta.edu>
2024-12-20 15:23:55 -0500

    Remove tex version from repo.

--- tex_version/code/hello_world.c
+++
@@ -1,7 +0,0 @@
-/* Courtesy of Brian Kernighan and https://en.wikipedia.org/wiki/%22Hello,_World!%22_program#C */
-#include <stdio.h>
-int main(void)
-{
-    printf("Hello, world\n");
-    return 0;
-}
